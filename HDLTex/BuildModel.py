57a5c35eda17aaf09d70b66b6f0c974936f13422
henan.wang <henan.wang@henan.wang's MacBook Pro>
2018-07-25 21:14:54 +0800

    change to 30 for test

--- HDLTex/BuildModel.py
+++ HDLTex/BuildModel.py
@@ -121,7 +121,7 @@ def buildModel_CNN(word_index, embeddings_index, nClasses, MAX_SEQUENCE_LENGTH, 
         convs = []
         filter_sizes = [3, 4, 5, 6, 7]
         for fsz in filter_sizes:
-            #l_conv = Conv1D(128, filter_length=fsz, activation='relu')(embedded_sequences)
+            #l_conv = Conv1D(128, filter_length=fsz, activation='relu')(embedded_sequences)  # old api
             l_conv = Conv1D(128, kernel_size=fsz, activation='relu')(embedded_sequences)
             l_pool = MaxPooling1D(5)(l_conv)
             convs.append(l_pool)
@@ -131,8 +131,8 @@ def buildModel_CNN(word_index, embeddings_index, nClasses, MAX_SEQUENCE_LENGTH, 
         l_cov1 = Conv1D(128, 5, activation='relu')(l_merge)
         l_pool1 = MaxPooling1D(5)(l_cov1)
         l_cov2 = Conv1D(128, 5, activation='relu')(l_pool1)
-        # l_pool2 = MaxPooling1D(30)(l_cov2)  # TODO why not right?
-        l_pool2 = MaxPooling1D(14)(l_cov2)
+        l_pool2 = MaxPooling1D(30)(l_cov2)  # TODO why not right?
+        #l_pool2 = MaxPooling1D(14)(l_cov2)
         l_flat = Flatten()(l_pool2)
         l_dense = Dense(128, activation='relu')(l_flat)
         preds = Dense(nClasses, activation='softmax')(l_dense)
