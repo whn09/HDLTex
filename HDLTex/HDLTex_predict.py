9264508438a1bdd3493e1221479080aca30ca97b
henan.wang <henan.wang@henan.wang's MacBook Pro>
2018-08-06 15:23:52 +0800

    add test_filename

--- HDLTex/HDLTex_predict.py
+++ HDLTex/HDLTex_predict.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     '''
 
     X_train, y_train, X_test, y_test, content_L2_Train, L2_Train, content_L2_Test, L2_Test, number_of_classes_L2, word_index, embeddings_index, number_of_classes_L1 = Data_helper.loadData_Tokenizer(
-        MAX_NB_WORDS, MAX_SEQUENCE_LENGTH, embedding_type='word2vec')
+        MAX_NB_WORDS, MAX_SEQUENCE_LENGTH, embedding_type='word2vec', is_train=False)
     print('X_train:', len(X_train), 'y_train:', len(y_train), 'X_test:', len(X_test), 'y_test:', len(y_test),
           'content_L2_Train:', len(content_L2_Train), 'L2_Train:', len(L2_Train), 'content_L2_Test:',
           len(content_L2_Test), 'L2_Test:', len(L2_Test), 'number_of_classes_L2:', number_of_classes_L2, 'word_index:',
